41d052ed7b90d201a7bf3d6973bc8a0ce6ddfb84
Your Name <you@example.com>
2024-08-11 17:35:50 +0100

    first commit

---
+++ boxStyle.js
@@ -0,0 +1,29 @@
+const box0 = document.getElementById("0");
+const box1 = document.getElementById("1");
+const box2 = document.getElementById("2");
+const box3 = document.getElementById("3");
+const box4 = document.getElementById("4");
+const box5 = document.getElementById("5");
+const box6 = document.getElementById("6");
+const box7 = document.getElementById("7");
+const box8 = document.getElementById("8");
+
+box0.style.borderTop = "none";
+box0.style.borderLeft = "none";
+
+box1.style.borderTop = "none";
+
+box2.style.borderTop = "none";
+box2.style.borderRight = "none";
+
+box3.style.borderLeft = "none";
+
+box5.style.borderRight = "none";
+
+box6.style.borderLeft = "none";
+box6.style.borderBottom = "none";
+
+box7.style.borderBottom = "none";
+
+box8.style.borderRight = "none";
+box8.style.borderBottom = "none";
